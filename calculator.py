77dea7d620d0fd07f6678c8d11337ebcbe08a84d
jiwookYG <island0318@gmail.com>
2020-11-09 15:43:31 +0900

    Add get_Remainder function

--- calculator.py
+++ calculator.py
@@ -1,15 +1,18 @@
 # 기본 계산기
 def add(a,b):
-  return a + b
+    return a + b
 
 def subtract(a, b):
-  return a - b
+    return a - b
 
 def multiply(a, b):
-  return a * b 
+    return a * b 
 
 def divide_new(a, b):
   return a / b
 
 def get_Median(a, b):
     return (a + b) / 2
+
+def get_Remainder(a, b):
+    return a//b
